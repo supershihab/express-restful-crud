5da4eba303c088db586ad3689e5a0e9beab84b36
Shihab Mahmud <shihabmahmud471@gmail.com>
2023-09-04 00:06:39 +0600

    GET single product by ID

--- server.js
+++ server.js
@@ -16,10 +16,22 @@ app.get('/blog', (req, res) => {
   res.send('Hello from Blog! Hooray!!');
 });
 
-app.get('/products', async(req, res) => {
+app.get('/products', async (req, res) => {
   try {
     const products = await Product.find({});
     res.status(200).json(products);
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message});
+  }
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
 
   } catch (error) {
     console.log(error.message);
