8202539f275bc1719f97149d1b29b07a186b8225
Shihab Mahmud <shihabmahmud471@gmail.com>
2023-09-03 23:57:48 +0600

    GET products from DB

--- server.js
+++ server.js
@@ -16,7 +16,18 @@ app.get('/blog', (req, res) => {
   res.send('Hello from Blog! Hooray!!');
 });
 
-app.post('/product', async (req, res) => {
+app.get('/products', async(req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message});
+  }
+});
+
+app.post('/products', async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
