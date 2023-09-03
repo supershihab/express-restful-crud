ed81ddeef9cec7c150d1654e6ce2ceae9c975ab1
Shihab Mahmud <shihabmahmud471@gmail.com>
2023-09-03 23:00:30 +0600

    Add Mongoose and MongoDB connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 //routes
@@ -7,8 +8,18 @@ app.get('/', (req, res) => {
   res.send('Hello Node API!');
 });
 
+app.get('/blog', (req, res) => {
+  res.send('Hello from Blog! Hooray!!');
+});
 
 app.listen(3000, () => {
   console.log('Node app is running on port 3000.');
 });
 
+mongoose
+.connect('mongodb+srv://admin:fuckoff1234@smapi.gzeh2ff.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+  console.log(`Connected to MongoDB!`);
+}).catch((error) => {
+  console.log(error);
+});
