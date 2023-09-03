9aed3a4d55d936f4f5b7894a54b0cefc7b70e6ef
Shihab Mahmud <shihabmahmud471@gmail.com>
2023-09-03 22:35:10 +0600

    initialize project files and install express

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Hello Node API!');
+});
+
+
+app.listen(3000, () => {
+  console.log('Node app is running on port 3000.');
+});
+
