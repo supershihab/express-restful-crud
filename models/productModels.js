99a150750656f2a9a2b9fabff36df077969309dd
Shihab Mahmud <shihabmahmud471@gmail.com>
2023-09-03 23:17:54 +0600

    Add Product model for MongoDB

---
+++ models/productModels.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+//product schema
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter a product name."],
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.get('/blog', (req, res) => {
   res.send('Hello from Blog! Hooray!!');
 });
 
-app.listen(3000, () => {
-  console.log('Node app is running on port 3000.');
-});
+mongoose.set("strictQuery", false);
 
 mongoose
 .connect('mongodb+srv://admin:fuckoff1234@smapi.gzeh2ff.mongodb.net/Node-API?retryWrites=true&w=majority')
 .then(() => {
-  console.log(`Connected to MongoDB!`);
+  app.listen(3000, () => {
+    console.log('Node app is running on port 3000.');
+  });
 }).catch((error) => {
   console.log(error);
 });
