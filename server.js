91a59ecd4f1d86adea74fd867ead95d9c2e555e5
DESKTOP-G6PTLPB\ShihabMahmud <shihabmahmud471@gmail.com>
2023-09-05 17:14:03 +0600

    Add CORS middleware

--- server.js
+++ server.js
@@ -4,18 +4,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const productRoute = require('./routes/productRoute');
 const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors');
 
 const app = express();
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 3000;
 
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
 //middleware - specify json middleware and form data middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
+// app.use(cors(corsOptions));
 
 //routes
-
 app.use('/api/products', productRoute);
 app.use(errorMiddleware);
 
